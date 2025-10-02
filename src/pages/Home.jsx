aa59cc09f1de8b8f50b7b9197545e1b22cc9e6f9
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2025-10-02 00:06:02 -0300

    feat: setup tailwind with custom theme and animations

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+  return (
+    <div className="min-h-screen">
+      {/* Theme toggle button */}
+
+      {/* Background Effects */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+    </div>
+  );
+};
